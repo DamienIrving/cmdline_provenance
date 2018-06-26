0123ff566ce70256b14df39925bc59bf28a89b13
Damien Irving <irving.damien@gmail.com>
2018-06-26 12:23:11 +1000

    Simpler selection of functions

--- cmdline_provenance/__init__.py
+++ cmdline_provenance/__init__.py
@@ -1,9 +1,8 @@
 """Utilities for capturing the history of commands used to produce a given output"""
 
-from .cmdline_provenance import new_cmdline_history
-from .cmdline_provenance import update_history
-from .cmdline_provenance import write_history_txt
+from .cmdline_provenance import new_log
+from .cmdline_provenance import write_log
 
-__all__ = [new_cmdline_history, update_history, write_history_txt]
+__all__ = [new_log, write_log]
 
 
--- cmdline_provenance/cmdline_provenance.py
+++ cmdline_provenance/cmdline_provenance.py
@@ -4,8 +4,8 @@ import datetime
 import git
 
 
-def new_cmdline_history(repo_dir=None):
-    """Create a new history record.
+def get_current_entry(repo_dir=None):
+    """Create a record of the current command line entry.
     
     Args:
       repo_dir (str, optional) : location of git repository for the script
@@ -31,62 +31,53 @@ def new_cmdline_history(repo_dir=None):
     return entry
 
     
-def update_history(file_info=None, extra_notes=None, repo_dir=None):
-    """Update command line history.
+def new_log(infile_history=None, extra_notes=None, repo_dir=None):
+    """Create a new command line log/history.
     
     Args:
-      file_info (dict, optional) : A dictionary where keys are input filenames and values are 
-        the command line history of those files 
+      infile_history (dict, optional) : A dictionary where keys are input filenames and values are 
+        the command line log/history of those files 
       extra_notes (list, optional) : List containing character strings of extra information 
         (output is one list item per line)
       repo_dir (str, optional) : Location of git repository for the script that was executed
          at the command line (so that git hash can be included in updated history)
       
     Returns:
-      str : complete updated command line history
+      str : complete updated command line log
       
     """
     
-    output = ''
+    log = ''
         
-    # Write the newest command line entry
-    new_history = new_cmdline_history(repo_dir=repo_dir)
-    output += new_history + '\n'
+    current_entry = get_current_entry(repo_dir=repo_dir)
+    log += current_entry + '\n'
     
-    # Write the extra info
     if extra_notes:
-        output += 'Extra notes: \n'
+        log += 'Extra notes: \n'
         for line in extra_notes:
-            output += line + '\n'
+            log += line + '\n'
     
-    # Write the file details
-    if file_info:
-        assert type(file_info) == dict
-        nfiles = len(list(file_info.keys()))
-        for fname, history in file_info.items():
+    if infile_history:
+        assert type(infile_history) == dict
+        nfiles = len(list(infile_history.keys()))
+        for fname, history in infile_history.items():
             if nfiles > 1:
-                output += 'History of %s: \n %s \n' %(fname, history)
+                log += 'History of %s: \n %s \n' %(fname, history)
             else:
-                output += '%s \n' %(history)
+                log += '%s \n' %(history)
     
-    return output
+    return log
     
     
-def write_history_txt(fname, file_info=None, extra_notes=None, repo_dir=None):
-    """Write an updated command line history to a text file.
+def write_log(fname, cmd_log):
+    """Write an updated command line log/history to a text file.
     
     Args:
-      file_info (dict, optional) : A dictionary where keys are input filenames and values are 
-        the command line history of those files 
-      extra_notes (list, optional) : List containing character strings of extra information 
-        (output is one list item per line)
-      repo_dir (str, optional) : Location of git repository for the script that was executed
-         at the command line (so that git hash can be included in updated history)
+      fname (str) : name of output file
+      cmdlog (str) : command line log produced by new_log()
     
     """
    
-    complete_history = update_history(file_info=file_info, extra_notes=extra_notes, repo_dir=repo_dir)
-   
-    history_file = open(fname, 'w')
-    history_file.write(complete_history) 
-    history_file.close()
+    log_file = open(fname, 'w')
+    log_file.write(cmd_log) 
+    log_file.close()

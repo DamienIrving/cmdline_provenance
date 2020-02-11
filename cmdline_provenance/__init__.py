975f5d12b06051158186c6b6981ac5c45f5cca7e
Zebedee Nicholls <zebedee.nicholls@climate-energy-college.org>
2020-02-11 19:09:56 +1100

    Add versioneer

--- cmdline_provenance/__init__.py
+++ cmdline_provenance/__init__.py
@@ -1,9 +1,17 @@
 """Utilities for capturing the history of commands used to produce a given output"""
 
+from ._version import get_versions
+
+__version__ = get_versions()['version']
+
+del get_versions
+
 from .cmdline_provenance import new_log
 from .cmdline_provenance import read_log
 from .cmdline_provenance import write_log
 
 __all__ = [new_log, read_log, write_log]
+
+
 
 
